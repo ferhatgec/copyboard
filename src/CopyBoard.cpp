d6f671a029325a90ffe6b2e8f8907f6c627a5512
ferhatgec <ferhatgectao@gmail.com>
2020-09-04 23:43:12 +0300

    Add Help & Version function.

--- src/CopyBoard.cpp
+++ src/CopyBoard.cpp
@@ -7,7 +7,34 @@
 
 #include <iostream>
 #include <cstring>
+
+/* Copyboard Parser */
 #include <Parser.hpp>
+
+/* const char [] */
+#define COPYBOARD_ALL_VERSION "0.1-beta-1"
+
+/*
+	TODO: Add Major, Minor, State of Copyboard version.
+*/
+
+static void Version() {
+	std::cout << "Fegeya Copyboard " << COPYBOARD_ALL_VERSION << "\n" <<
+	"Fegeya Copyboard licensed under the terms of MIT License.\n";
+}
+
+/*
+	Fegeya Copyboard <version>
+	./copyboard --list || --la || --list_all : List board
+	./copyboard ....
+*/
+static void Help(char **arg) {
+	std::cout << "Fegeya Copyboard " << COPYBOARD_ALL_VERSION << "\n" <<
+	arg[0] << " --list || --la || --list_all : List board\n" <<
+	arg[0] << " --add : Add new text in board\n" <<
+	arg[0] << " --version || --v : Show version\n" <<
+	arg[0] << " --help || --h : Show help (here)\n";
+}
 
 static void Add() {
 	std::string tag, data;
@@ -40,6 +67,10 @@ int main(int argc, char **argv) {
 		cb.ListAll();
      	   else if(reg == "--add")
 		Add();
+	   else if(reg == "--help" || reg == "--h")
+	   	Help(argv);
+	   else if(reg == "--version" || reg == "--v")
+	   	Version();
            else
     		std::cout << "Undefined argument.";
     }
